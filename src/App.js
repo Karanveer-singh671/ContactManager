ad4629135b4b251fc56ab05817871da6bf54024f
Karanveer Singh <singhkaranveer671@gmail.com>
2019-03-14 14:43:16 -0700

    setting up Router for routes and made about page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+// stores routes, switch for not found page
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Contact from './components/contacts/Contacts';
 import AddContact from './components/contacts/addContact';
 import Header from './components/layout/Header';
+import About from './components/layout/About';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from './context';
 
@@ -11,13 +14,20 @@ class App extends Component {
 		// wrap entire main app in Provider
 		return (
 			<Provider>
-				<div className="App">
-					<Header branding="Contact Manager" />
-					<div className="container">
-						<AddContact />
-						<Contacts />
+				<Router>
+					<div className="App">
+						<Header branding="Contact Manager" />
+						<div className="container">
+							<Switch>
+								<Route exact path="/" component={Contacts} />
+								<Route exact path="/contact/add" component={AddContact} />
+								<Route exact path="/about" component={About} />
+							</Switch>
+							<AddContact />
+							<Contacts />
+						</div>
 					</div>
-				</div>
+				</Router>
 			</Provider>
 		);
 	}
